cdcbd9254b095785674b6a06a8ea3a032118ca36
Rodrigo Dirube <rodrigodirube5@gmail.com>
2021-09-29 12:31:23 -0300

    among us break

--- src/app/gnomes-game/components/tutorial/tutorial.component.ts
+++ src/app/gnomes-game/components/tutorial/tutorial.component.ts
@@ -12,7 +12,7 @@ import { GnomeInfo, GnomeScene, GnomeInfoTutorial } from '../../models/types';
 import { SurpriseAnimationInfo } from '../../models/types';
 
 import { GnomesChallengeService } from 'src/app/shared/services/gnomes-challenge.service';
-
+import anime from 'animejs'
 import { SubscriberOxDirective } from 'micro-lesson-components';
 import { AnswerMetric, OxTextInfo } from 'ox-types';
 import { GnomeAnswerService } from 'src/app/shared/services/gnome-answer.service';
@@ -21,6 +21,7 @@ import { filter } from 'rxjs/operators';
 import { GnomeComponent } from '../gnome/gnome.component';
 import { PreloaderOxService } from 'ox-core';
 import { Typographies } from 'typography-ox';
+// import { threadId } from 'worker_threads';
 
 
 
@@ -34,17 +35,20 @@ import { Typographies } from 'typography-ox';
 export class TutorialComponent extends SubscriberOxDirective implements OnInit, AfterViewInit {
   @ViewChildren(GnomeComponent) gnomeComponents: QueryList<GnomeComponent>;
 
-  firstSecuenceTrigger: EventEmitter<any> = new EventEmitter();
+  thirdSecuence: EventEmitter<any> = new EventEmitter();
+  secondSecuence: EventEmitter<any> = new EventEmitter();
+  selectTrigger: EventEmitter<any> = new EventEmitter()
 
   currentStatus: GnomeSceneStatus = 'ver';
   public gnomesTutorial: GnomeInfoTutorial[] = [];
   public allGnomes: GnomeInfoTutorial[] = [];
   public sceneSvg: string;
   public surpriseInfo: SurpriseAnimationInfo;
-  public sequence: string[] = [];
+  public sequence: GnomeInfoTutorial[] = [];
   public currentScenePositions = [];
   public sequenceSubscription: Subscription;
-  public info: {
+  public info: 
+  {
     gnomes: GnomeInfoTutorial[],
     scenes: GnomeScene[]
   };
@@ -52,22 +56,65 @@ export class TutorialComponent extends SubscriberOxDirective implements OnInit, 
   public gnomeClass: any[];
   public gnomesTutorialText: OxTextInfo[];
   public tutorialTextContent: string;
+  public thirdSecuenceVar:boolean = true;
+  public sequenceCounter: number = 0;
+  public stepTutorial:number = 2;
+  public fourthWatchVar:boolean = false;
+
+
 
   constructor(private challengeService: GnomesChallengeService, private answerService: GnomeAnswerService, private hintService: HintService, private preLoaderServide: PreloaderOxService) {
     super()
     this.addSubscription(this.challengeService.currentExercise.pipe(filter(x => x === undefined)), z => {
       this.info = JSON.parse(this.preLoaderServide.getResourceData('gnome-game/jsons/gnomes-and-scenes-info.json'));
-      this.sceneSvg = 'gnome-game/svg/Fondos/jardin-establo-4.svg';
+      this.sceneSvg = 'gnome-game/svg/Fondos/jardin-chimenea-2.svg';
       this.allGnomes = this.info.gnomes.map(gnome => {
         return { color: gnome.color, sound: gnome.sound, reference: gnome.reference, selectAvaible: false };
       });
-      this.gnomesTutorial = [this.allGnomes[0], this.allGnomes[1], this.allGnomes[2], this.allGnomes[3]];
-      this.currentScenePositions = this.info.scenes[6].positions;
+      this.gnomesTutorial = [this.allGnomes[0], this.allGnomes[1]];
+      this.currentScenePositions = this.info.scenes[3].positions;
+    })
+    this.addSubscription(this.selectTrigger, x => {
+      if (this.sequence.length > 0) {
+        this.gnomeToSelect();
+      }
+      else if (this.stepTutorial === 2) {
+        this.stepTutorial++;
+        this.secondSecuence.emit();
+        
+      }
+      else if(this.stepTutorial === 3){
+        this.thirdSecuence.emit();
+        this.stepTutorial++;
+        console.log(this.stepTutorial)
+      } else if (this.stepTutorial === 4) {
+           this.secuenceMethod4();
+           console.log(this.fourthWatchVar)
+      }
+    })
+    this.addSubscription(this.secondSecuence, z => {
+      if(this.sequence.length < 2) {
+       setTimeout(() => {
+         this.sequenceMethod2()
+       }, 1000);
+      } else {
+        setTimeout(() => {
+          this.selectTrigger.emit();
+        }, 1600);
+      }
+    })
+    this.addSubscription(this.thirdSecuence, z => {
+      if (this.sequence.length < 3) {
+        setTimeout(() => {
+          this.sequenceMethod3();
+        }, 1000);
+      } else {
+        setTimeout(() => {
+          this.selectTrigger.emit()
+        }, 1600);
+      }
     })
 
-    this.addSubscription(this.firstSecuenceTrigger, z => {
-      this.secondSecuence();
-    })
 
   }
   // [{"x":"8.5vh","y":"11vh"},{"x":"8vh","y":"53vh"},{"x":"98vh","y":"11vh"},{"x":"98vh","y":"53vh"}]
@@ -77,58 +124,93 @@ export class TutorialComponent extends SubscriberOxDirective implements OnInit, 
   ngOnInit(): void {
   }
 
+
+
   ngAfterViewInit(): void {
-    this.startTutorial();
-  }
-
-
-  startTutorial() {
-    setTimeout(() => {
-      this.gnomeComponents.toArray()[0].playAudioTutorial();
-    }, 1000);
-    setTimeout(() => {
-      this.gnomesTutorial[0].selectAvaible = true;
-    }, 1700);
+    this.sequenceMethod1();
+    this.gnomeToSelect();
   }
 
 
 
-
-  secuencialTutorial(i) {
-    this.gnomeComponents.toArray()[i].playAudioTutorial();
-    this.gnomeComponents.toArray()[i].gnomeInfoTutorial.selectAvaible = false;
-    this.firstSecuenceTrigger.emit();
+  sequenceMethod1() {
+    this.sequence.push(this.gnomesTutorial[0]);
+    this.gnomeComponents.toArray()[0].playAudioTutorial();
   }
 
 
 
-  secondSecuence() {
-    for (let i = 0; i < 3; i++) {
-      setTimeout(() => {
-        this.gnomeComponents.toArray()[i].playAudioTutorial();
-        this.sequence.push(this.gnomesTutorial[i].reference)
-      }, 1000 * i + 1)  
+  sequenceMethod2() {
+    if (this.sequenceCounter < 2) {
+      this.sequence.push(this.gnomesTutorial[this.sequenceCounter]);
+      const indexToSelect = this.gnomesTutorial.indexOf(this.sequence[this.sequenceCounter])
+      this.gnomeComponents.toArray()[indexToSelect].playAudioTutorial();
+      this.sequenceCounter++;
     }
-    
-    console.log(this.sequence);
+    this.secondSecuence.emit();
+
+    // if (this.secondVarSeq) {
+    //   this.sequence.push(this.gnomesTutorial[0]);
+    //   this.gnomeComponents.toArray()[0].playAudioTutorial();
+    //   this.secondVarSeq = false;
+    // } else {
+    //   this.sequence.push(this.gnomesTutorial[1]);
+    //   this.gnomeComponents.toArray()[1].playAudioTutorial();
+    // }
   }
 
-  //   }
-  //   setTimeout(() => {
-  //   }, 1000);
-  //   setTimeout(() => {
-  //     this.gnomeComponents.toArray()[2].playAudioTutorial();
-  //   }, 2000);
-  //   setTimeout(() => {
-  //     this.gnomeComponents.toArray()[1].playAudioTutorial();
-  //   }, 3000);
-  // }
 
 
-  secondSecuenceSolved(i) {
-    this.answerService.addPartialAnswer(i)
 
+  sequenceMethod3() {
+      this.sequenceCounter = 0;
+    setTimeout(() => {
+      if (this.thirdSecuenceVar) {
+        this.sequence.push(this.gnomesTutorial[0]);
+        this.gnomeComponents.toArray()[0].playAudioTutorial();
+        this.thirdSecuenceVar = !this.thirdSecuenceVar;
+      } else {
+        this.sequence.push(this.gnomesTutorial[1]);
+        this.gnomeComponents.toArray()[1].playAudioTutorial();
+      }
+      this.thirdSecuence.emit()
+   }, 700);
   }
+
+
+  secuenceMethod4 () {
+    this.fourthWatchVar = true;
+  }
+
+
+
+  gnomeToSelect() {
+    const indexToSelect = this.gnomesTutorial.indexOf(this.sequence[0])
+    this.gnomesTutorial[indexToSelect].selectAvaible = true;
+  }
+
+
+
+  sequenceAnswer(i) {
+    // const indexToSelect = this.gnomesTutorial.indexOf(this.sequence[0])
+    if (this.gnomeComponents.toArray()[i].gnomeInfoTutorial.selectAvaible === true) {
+      this.gnomeComponents.toArray()[i].gnomeInfoTutorial.selectAvaible = false;
+      this.gnomeComponents.toArray()[i].playAudioTutorial();
+      this.sequence.shift();
+      this.selectTrigger.emit();
+    }
+  }
+
+
 
 
 }
+
+
+
+
+
+
+
+
+
